3f41dcbbc152aa421fe53213ed68b47f8a5752d0
riaprasad <riaprasad46@gmail.com>
2021-07-26 12:09:49 +0530

    all css changes done

--- src/InstructionPage.jsx
+++ src/InstructionPage.jsx
@@ -8,9 +8,18 @@ import { useState } from 'react'
 const InstructionPage = ({showMode}) => {
     return (
         <div className="btnGroup">
+        <p>
+            <ul>
+                <li>Every square has to contain a single number</li>
+                <li>Only the numbers from 1 through to 9 can be used</li>
+                <li>Each 3×3 box can only contain each number from 1 to 9 once</li>
+                <li>Each vertical column can only contain each number from 1 to 9 once</li>
+                <li>Each horizontal row can only contain each number from 1 to 9 once</li>
+            </ul>
+        </p>
         <button className="btn-2" onClick={showMode}>
          <span>
-             You are reading instructions
+             Main Page
         </span> 
         </button>
        
