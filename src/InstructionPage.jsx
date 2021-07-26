2c999dc8aa477aae6faef5865b86b20ab6c04f84
Siddhant <siddhant.roy2019@vitstudent.ac.in>
2021-07-26 17:04:46 +0530

    Styles fixed

--- src/InstructionPage.jsx
+++ src/InstructionPage.jsx
@@ -4,20 +4,22 @@ import './App.css'
 const InstructionPage = ({ showGame }) => {
   return (
     <div className="btnGroup">
-      <p className="instruct">
-        <ul>
-          <li>Every square has to contain a single number</li>
-          <li>Only the numbers from 1 through to 9 can be used</li>
-          <li>Each 3×3 box can only contain each number from 1 to 9 once</li>
-          <li>
-            Each vertical column can only contain each number from 1 to 9 once
-          </li>
-          <li>
-            Each horizontal row can only contain each number from 1 to 9 once
-          </li>
-        </ul>
-      </p>
-      <button className="btn-2" onClick={showGame}>
+      <div className="koko">
+        <p className="instruct">
+          <ul>
+            <li>Every square has to contain a single number</li>
+            <li>Only the numbers from 1 through to 9 can be used</li>
+            <li>Each 3×3 box can only contain each number from 1 to 9 once</li>
+            <li>
+              Each vertical column can only contain each number from 1 to 9 once
+            </li>
+            <li>
+              Each horizontal row can only contain each number from 1 to 9 once
+            </li>
+          </ul>
+        </p>
+      </div>
+      <button className="btn-3" onClick={showGame}>
         <span>Play!</span>
       </button>
     </div>
